f823978831b3b16ef02c9669a94c76b70932f097
Luiz Alexandre Ferreira Martinez <alxmartinez@gmail.com>
2019-10-24 19:39:54 -0300

    Update with new versions of 1-Introduction , 2-Variables

---
+++ 1-Introduction.js
@@ -0,0 +1,111 @@
+
+console.log(5); 
+
+console.log(48);
+
+console.log(4);
+
+// Prints 5 to the console
+console.log(5);
+
+console.log(5);  // Prints 5
+
+/*
+This is all commented 
+console.log(10);
+None of this is going to run!
+console.log(99);
+*/
+
+console.log(/*IGNORED!*/ 5); // Still just prints 5 
+
+// Opening Line
+console.log('It was love at first sight.');
+
+/* console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+console.log('Location of Codecademy headquarters: 575 Broadway, New York City');
+
+console.log(40);
+
+console.log('JavaScript');
+
+console.log(2011);
+
+console.log('Woohoo! I love to code! #codecademy');
+
+console.log(20.49);
+
+
+console.log(3 + 4); // Prints 7
+console.log(5 - 1); // Prints 4
+console.log(4 * 2); // Prints 8
+console.log(9 / 3); // Prints 3
+
+
+console.log('hi' + 'ya'); // Prints 'hiya'
+
+console.log('wo' + 'ah'); // Prints 'woah'
+
+console.log('I love to ' + 'code.')
+// Prints 'I love to code.'
+
+
+console.log('Hello' + 'World');
+
+console.log('Hello ' + 'World');
+
+console.log('front ' + 'space'); 
+// Prints 'front space'
+
+console.log('back' + ' space'); 
+// Prints 'back space'
+
+console.log('no' + 'space'); 
+// Prints 'nospace'
+
+console.log('middle' + ' ' + 'space'); 
+// Prints 'middle space'
+
+
+console.log('Hello'.length); // Prints 5
+
+console.log('Teaching the world how to code'.length);  // Prints 30
+
+
+console.log('hello'.toUpperCase()); // Prints 'HELLO'
+
+console.log('Hey'.startsWith('H')); // Prints true
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+
+console.log(Math.random()); // Prints a random number between 0 and 1
+
+console.log(Math.random() * 50);  // To generate a random number between 0 and 50, we could multiply this result by 50
+
+console.log(Math.random() * 100);
+
+console.log(Math.floor(Math.random() * 50));
+// Math.floor() takes a decimal number, and rounds down to the nearest whole number. 
+// You can use Math.floor() to round down a random number like this:
+
+console.log(Math.floor(Math.random() * 100));
+
+console.log(Math.ceil(43.8));  // resultado= 44
+
+console.log(Number.isInteger(2017));    // resultado = true
+
+
+
+
