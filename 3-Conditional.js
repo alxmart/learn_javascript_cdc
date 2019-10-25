84c76da690160b29776fbae93e78ee384eb2eb70
Luiz Alexandre Ferreira Martinez <alxmartinez@gmail.com>
2019-10-25 18:13:56 -0300

    Updating 3-Conditional with more code examples

--- 3-Conditional.js
+++ 3-Conditional.js
@@ -194,7 +194,8 @@ if (isLocked) {
 }
 */
 let isLocked = false;
-isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+isLocked ? console.log('You will need a key to open the door.') 
+         : console.log('You will not need a key to open the door.');
 //----------------------------------------------------------------
 
 /*  ORIGINAL:
@@ -280,7 +281,28 @@ switch (groceryItem) {
 
 // ------------------------------------------------------
 
+let athleteFinalPosition = 'first place';
 
+switch (athleteFinalPosition) {
+
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+    
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+
+  default:
+    console.log('No medal awarded.');
+    break;
+}
+
+// ---------------------------------------------------
 
 
 
--- notes.js
+++ notes.js
@@ -1,67 +1,6 @@
 
 
 
-/*    10>  The Switch Keyword
-----------------------------------------*/
-
-let groceryItem = 'papaya';
-
-if (groceryItem === 'tomato') {
-  console.log('Tomatoes are $0.49');
-} else if (groceryItem === 'papaya'){
-  console.log('Papayas are $1.29');
-} else {
-  console.log('Invalid item');
-}
-
-// -----------------------------------------
-
-let groceryItem = 'papaya';
-
-switch (groceryItem) {
-  case 'tomato':
-    console.log('Tomatoes are $0.49');
-    break;
-  case 'lime':
-    console.log('Limes are $1.49');
-    break;
-  case 'papaya':
-    console.log('Papayas are $1.29');
-    break;
-  default:
-    console.log('Invalid item');
-    break;
-}
-
-// Prints 'Papayas are $1.29'
-
-// -----------------------------------------------
-
-let athleteFinalPosition = 'first place';
-
-switch (athleteFinalPosition) {
-
-  case 'first place':
-    console.log('You get the gold medal!');
-    break;
-
-  case 'second place':
-    console.log('You get the silver medal!');
-    break;
-
-  case 'third place':
-    console.log('You get the bronze medal!');
-    break;
-
-  default:
-    console.log('No medal awarded.');
-    break;
-}
-
-// ---------------------------------------------------
-
-
-
 
 
 
